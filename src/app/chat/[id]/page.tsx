d1508ed2c57277e7a94059512a8fe686929493eb
Martin <martin.jokl123@gmail.com>
2026-04-08 18:33:59 +0200

    Add a back button

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -2,6 +2,7 @@
 import MessageContainer from '@/components/MessageContainer'
 import MessageInput from '@/components/MessageInput';
 import { getUser } from '@/dal/user';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 export default async function Chat({
@@ -19,10 +20,17 @@ export default async function Chat({
   }
 
   return (
-    <div className='h-screen flex flex-col p-8 w-300 mx-auto'>
-      <h1 className="text-center text-2xl mb-12">{recieverName}</h1>
-      <MessageContainer reciever={id === 'global' ? null : id} />
-      <MessageInput reciever={id === 'global' ? null : id} />
-    </div>
+    <>
+      <Link
+        className='absolute top-4 left-4 bg-bg-light px-4 py-2 block mx-auto mt-1 border rounded-md border-border hover:bg-border transition'
+        href="/"
+      >Back</Link>
+
+      <div className='h-screen flex flex-col p-8 w-300 mx-auto'>
+        <h1 className="text-center text-2xl mb-12">{recieverName}</h1>
+        <MessageContainer reciever={id === 'global' ? null : id} />
+        <MessageInput reciever={id === 'global' ? null : id} />
+      </div>
+    </>
   );
 }
