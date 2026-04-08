b3bea46a1d64aae58e95a5e61b19dd28461dba90
Martin <martin.jokl123@gmail.com>
2026-04-08 10:38:19 +0200

    Add better auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <></>
+    <>
+      <h1 className="text-center mt-8 text-2xl">Home page</h1>
+    </>
   );
 }
---
+++ src/db/drizzle.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import * as authSchema from './schema/auth-schema';
+//import * as mySchema from './schema/schema';
+
+const schema = {
+  ...authSchema,
+  //...mySchema
+}
+
+const db = drizzle(process.env.DATABASE_URL!, { casing: 'snake_case', logger: true, schema, mode: 'default' });
+
+export default db;
